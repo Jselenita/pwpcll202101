a3436a44744722860e1be2bb36bd44591b708c6f
Jessica <jessimm97@gmail.com>
2021-03-29 13:56:57 -0600

    proyecto en estado Inicial

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author:'Jessica Selene Mondragón Martínez',appName:'WebApp' });
+});
+//Agregar Ruta *7
+router.get('/greeting',function(req, res, next){
+  res.send('Jessica Mondragón <3')
+})
+
+module.exports = router;
